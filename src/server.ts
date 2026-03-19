8dc1aaa05913ec123f88369d9c0db2295555d171
Juan Carlos Gomez Jaramillo <juangomez88@gmail.com>
2026-03-19 14:13:41 -0500

    first commit

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express from 'express' 
+import colors from 'colors'
+import morgan from 'morgan'
+import { db } from './conifg/db'
+
+async function connectDB() {
+      try{
+            await db.authenticate()
+            db.sync()
+            console.log(colors.blue.bold('Conexión existosa a la BD'))
+      }catch(error){
+            //console.log(error)
+            console.log(colors.red.bold('Falló la conexión a la BD'))
+      }
+}
+
+connectDB()
+
+const app = express()
+
+app.use(morgan('dev'))
+
+app.use(express.json())
+
+
+
+export default app
